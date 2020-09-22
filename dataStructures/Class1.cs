e02043c15c07430ecb60a0ac705804ab3aabec82
Grace Bazneh <grace@bazneh.co.uk>
2020-09-22 14:50:31 +0100

    test commit

---
+++ dataStructures/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dataStructures
+{
+    class Class1
+    {
+    }
+}
---
+++ dataStructures/Stack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dataStructures
+{
+    class Stack
+    {
+        int[] stack;
+        int sp;
+        int max;
+
+        public Stack(int size)
+        {
+            max = size;
+            stack = new int[max];
+            sp = 0;
+        }
+      
+        public void Push(int item)
+        {
+
+        }
+    
+
+        public int Pop()
+        {
+            return 0;
+        }
+    }
+}
