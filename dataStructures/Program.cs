9549b6bc781840559f2c67bcf7b53182a4839f26
Grace Bazneh <15BaznehG@wg6.co.uk>
2020-09-22 13:51:52 +0100

    Add project files.

---
+++ dataStructures/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
